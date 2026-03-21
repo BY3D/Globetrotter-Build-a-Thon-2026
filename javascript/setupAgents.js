2e7626d66fb74b35aefc28da65b80b152b9d1590
BY3D <49314704+BY3D@users.noreply.github.com>
2026-03-21 11:09:56 -0400

    Add model loading

--- javascript/setupAgents.js
+++ javascript/setupAgents.js
@@ -4,22 +4,35 @@ import { FoundryLocalManager } from "foundry-local-sdk";
 const LLM_ALIAS = "phi-4-mini";
 const LLM_SIZE = "4 GB"
 
+// Create the Foundry Local service
 console.log("Instantiate Foundry Local service")
 FoundryLocalManager.create({ appName: "GlobeSpinner" });
 const manager = FoundryLocalManager.instance;
 await manager.startWebService();
 
+// Download the model if it's not on disk
 const catalog = manager.catalog;
 const model = await catalog.getModel(LLM_ALIAS);
 if (model.isCached) {
     console.log(`${LLM_ALIAS} has already been downloaded.`);
 } else{
     console.log(`Download LLM ${LLM_ALIAS}, 
-        may take up to ${LLM_SIZE} of disk space`);
+        may take up to ${LLM_SIZE} of disk space.`);
     await model.download();
 }
-if (!model.isCached) {
-    console.log("Downloading model: phi-4-mini...");
-    await model.download(); // Phi-4-mini is 3.86 GB
-}
+
+// Load the model
+console.log(`Loading ${LLM_ALIAS}...`);
 await model.load();
+const modelId = model.id;
+console.log(`${modelId} is ready.`);
+
+// OpenAI client allows communication with the LLMs.
+// It's pointing to the Foundry Local service.
+// However, it can point to an Azure service instead for online LLM processing
+const client = new OpenAI({
+  baseURL: manager.urls[0] + "/v1",
+  apiKey: "foundry-local",
+});
+
+export { client, modelId, MODEL_ALIAS };
